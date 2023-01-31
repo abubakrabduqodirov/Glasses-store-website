bdbb8b15574b3c6fc7ef01c4fa9642d8b3959996
abubakrabduqodirov <abubakirabduqodirov@gmail.com>
2023-01-31 21:25:05 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// const items = [
+//     { name: "book", price: 12000 },
+//     { name: "kopybook", price: 220 },
+//     { name: "cd", price: 10 },
+//     { name: "pensil", price: 4000 },
+//     { name: "pan", price: 500 },
+//     { name: "phone", price: 100 },
+// ];
+
+// const nums = [4, 7, 10];
+// const result = items.reduce((currentValue, item) => {
+//     return currentValue + item.price;
+// }, 0)
+// console.log(result);
